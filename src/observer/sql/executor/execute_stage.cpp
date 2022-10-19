2f4604641dbdbaebd34144ad8603b6281917988f
DevilExileSu <11782877+vvanish@user.noreply.gitee.com>
2022-10-19 20:55:33 +0800

    fix(show-index): output FAILURE when return error

--- src/observer/sql/executor/execute_stage.cpp
+++ src/observer/sql/executor/execute_stage.cpp
@@ -545,9 +545,13 @@ RC ExecuteStage::do_show_index(SQLStageEvent *sql_event) {
   Db *db = session_event->session()->get_current_db();
   const ShowIndex &show_index = sql_event->query()->sstr.show_index;
   Table *table = db->find_table(show_index.relation_name);
+  if (nullptr ==table) {
+    session_event->set_response("FAILURE\n");
+    return RC::SCHEMA_TABLE_NOT_EXIST;
+  }
   std::stringstream ss;
   RC rc = table->show_index(ss);
-  sql_event->session_event()->set_response(ss.str().c_str());
+  sql_event->session_event()->set_response(rc == RC::SUCCESS ? ss.str().c_str() : "FAILURE\n");
   return rc;
 }
 
